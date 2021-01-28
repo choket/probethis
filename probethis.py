bed52d741f45d97f0b439ca3a7e0333445c57dc6
Stefan Stojanovski <stefan@viris.eu>
2021-01-28 10:06:36 +0100

    Customized probethis to have better output

--- probethis.py
+++ probethis.py
@@ -6,77 +6,115 @@ import sys
 import os
 import re
 import time
+import socket
 import threading
 from colors import *
 
-psmall = ["81", "591", "2082", "2087", "2095", "2096", "3000", "8000", "8001", "8008", "8080", "8083", "8443", "8834", "8888"]
-plarge = ["81", "300", "591", "593", "832", "981", "1010", "1311", "2082", "2087", "2095", "2096", "2480", "3000", "3128", "3333", "4243", "4567", "4711", "4712", "4993", "5000", "5104", "5108", "5800", "6543", "7000", "7396", "7474", "8000", "8001", "8008", "8014", "8042", "8069", "8080", "8081", "8088", "8090", "8091", "8118", "8123", "8172", "8222", "8243", "8280", "8281", "8333", "8443", "8500", "8834", "8880", "8888", "8983", "9000", "9043", "9060", "9080", "9090", "9091", "9200", "9443", "9800", "9981", "12443", "16080", "18091", "18092", "20720", "28017"]
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+#psmall = ["80", "81", "443", "591", "2082", "2087", "2095", "2096", "3000", "8000", "8001", "8008", "8080", "8083", "8443", "8834", "8888"]
+psmall = ["80", "443",  "2082", "2087", "2095", "2096", "8080", "8083", "8443"]
+plarge = ["80", "81", "443", "300", "591", "593", "832", "981", "1010", "1311", "2082", "2087", "2095", "2096", "2480", "3000", "3128", "3333", "4243", "4567", "4711", "4712", "4993", "5000", "5104", "5108", "5800", "6543", "7000", "7396", "7474", "8000", "8001", "8008", "8014", "8042", "8069", "8080", "8081", "8088", "8090", "8091", "8118", "8123", "8172", "8222", "8243", "8280", "8281", "8333", "8443", "8500", "8834", "8880", "8888", "8983", "9000", "9043", "9060", "9080", "9090", "9091", "9200", "9443", "9800", "9981", "12443", "16080", "18091", "18092", "20720", "28017"]
 domains = []
 outputbuffer = []
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36'}
 
-def print_line(r, url, status_codes):
+def print_line(r, ip_addr):
     try:
-        size = "{}B".format(len(r.text))
         status = r.status_code
-        # set redirect value to location header if status code is 3xx else ''
-        redirect_value = lgreen+"-> {}".format(r.headers['location']) if str(status)[0] == "3" else ''
-        title = re.findall(r'<title[^>]*>(.*?)</title>', r.text)
-        title = '' if title == [] else title[0][:40]
+        url = r.url
+
+        # set redirect value to location header if status code is 3xx
+        redirect_value = r.headers['location'] if str(status)[0] == "3" else ''
+
+        title = re.findall(r'<title[^>]*>(.*?)</title>', r.text, re.IGNORECASE)
+        title = '' if title == [] else title[0]
+
+        if str(status)[0] == "1":
+            status_color = white
+        elif str(status)[0] == "2":
+            status_color = lgreen
+        elif str(status)[0] == "3":
+            status_color = lyellow
+        elif str(status)[0] == "4":
+            status_color = lred
+        elif str(status)[0] == "5":
+            status_color = lcyan
+
+        
         # Use dark yellow for 404
-        scolor = lyellow if r.status_code == 404 else statuscolors[int(str(status)[0])]
-        if not status_codes:
-            # if custom status codes are not provided output all urls
-            outputbuffer.append(url)
-        else:
-            # output only links with provided status codes
-            if status in status_codes:
-                outputbuffer.append(url)
-        # if status is 3xx then display location header value instead of title
-        title = title if redirect_value == '' else redirect_value
-        print("{:<42}".format(url),scolor,"[{}]".format(status),cyan,"{:<9}".format(size),end,white,"{:<30}".format(title),end)
+        outputbuffer.append(url)
+
+        print(url,
+              "[{}{}{}]".format(status_color, status, white),
+              "[{}{}{}]".format(cyan, redirect_value, white),
+              "[{}{}{}]".format(magenta, title, white),
+              "[{}]".format(ip_addr),
+              flush=True
+         )
     except Exception as e:
+        print(str(e))
         pass
 
-def remove_proto(url):
+def extract_domain(url):
     # remove http and https from input to avoid confusion
     if url.startswith('https://'):
         url = url[8:]
     elif url.startswith('http://'):
         url = url[7:]
+
+    url.rstrip("/")
+
     return url
 
-def work(timeout, ports, is_https, status_codes):
+def work(timeout, ports, is_https):
     while domains != []:
         try:
             domain = domains.pop()
-            domain = remove_proto(domain)
-            # if prefer https is selected
-            if is_https:
-                url = "https://{}".format(domain)
+            domain = extract_domain(domain)
+            
+            if domain == "":
+                continue
+
+            try:
+                ip = socket.gethostbyname(domain)
+            except socket.gaierror:
+                pass
+
+            for port in ports:
+                resp_http = None
+                resp_https = None
+
+                url_http = "http://{}:{}".format(domain, port)
+                url_https = "https://{}:{}".format(domain, port)
+
+                # THis doesnt work
                 try:
-                    r = requests.get(url, timeout=timeout, allow_redirects=False, headers=headers)
-                # if https fails try http
-                except requests.exceptions.SSLError:
-                    url = "http://{}".format(domain)
-                    r = requests.get(url, timeout=timeout, allow_redirects=False, headers=headers)
-                    print_line(r, url, status_codes)
-            else:
-                # do both http and https
-                url = "http://{}".format(domain)
-                r = requests.get(url, timeout=timeout, allow_redirects=False, headers=headers)
-                print_line(r, url, status_codes)
-                url = "https://{}".format(domain)
-                r = requests.get(url, timeout=timeout, allow_redirects=False, headers=headers)
-                print_line(r, url, status_codes)
-            if ports:
-                # scan for ports if specified
-                for port in ports:
-                    url = "http://{}:{}".format(domain,port)
-                    r = requests.get(url, timeout=timeout, allow_redirects=False, headers=headers)
-                    print_line(r, url, status_codes)
+                    resp_https = requests.get(url_https, timeout = timeout, allow_redirects = False,
+                                            headers = headers, stream=True, verify=False)
+                except requests.ConnectionError:
+                    # Only check for http listener if https fails
+                    try:
+                        resp_http = requests.get(url_http, timeout = timeout, allow_redirects = False,
+                                                headers = headers, stream=True)
+                    except requests.ConnectionError:
+                        pass
+
+                
+                if resp_https is not None:
+                    print_line(resp_https, ip)
+                
+                # Print http only if is_https is false, or if there is no https response
+                if resp_http and (not is_https or resp_https is None):
+                    print_line(resp_http, ip)
+
         except Exception as e:
+            raise e
             pass
+
+
 
 def main():
     print(white,'''
@@ -85,7 +123,7 @@ def main():
   /_)_/ (_(_)/_)</_<__/ /_<_/_)_
  /              ''',end,'''@github/shobhit99''',white,'''
 '              
-    ''',end)
+    ''',end, file=sys.stderr)
     usage = '''
     Example:
 
@@ -97,8 +135,7 @@ def main():
     parser.add_argument("-f", help="File with domain list", type=str, action="store")
     parser.add_argument("-t", help="No of threads", type=int, default=5, action="store")
     parser.add_argument("-o", help="Output file name, Saves all status codes by default unless codes specified using -s", type=str, action="store")
-    parser.add_argument("-s", help="Output urls with only specified status codes seperated by comma", type=str, action="store")
-    parser.add_argument("-p", help="[ Custom Ports seperated by comma | small | large ] small and large uses predefined list of ports", type=str, action="store")
+    parser.add_argument("-p", help='"Comma seperated list of ports, or "small" or "large" for a predefined list of ports', type=str, action="store", default="small")
     parser.add_argument("--https", help="Prefer HTTPS over http", action="store_true")
     parser.add_argument("--timeout", help="Timeout for requests", type=int, default=5, action="store")
     args = parser.parse_args()
@@ -106,17 +143,17 @@ def main():
     threadcount = args.t
     outputfile = args.o
     ports = args.p
-    status_codes = args.s
     is_https = args.https
     timeout = args.timeout
-    if ports:
+
+    if ports == "small":
+        ports = psmall
+    elif ports == "large":
+        ports = plarge
+    else:
+        ports.replace(" ", "")
         ports = ports.split(",")
-    if status_codes:
-        if not outputfile:
-            sys.exit("Please provide output file name with -o")
-        else:
-            status_codes = status_codes.split(",")
-            status_codes = [int(i) for i in status_codes] 
+
     # if pipe input
     if not file:
         for d in sys.stdin:
@@ -126,19 +163,11 @@ def main():
         data = open(file, "r").read()
         domains.extend(data.split('\n'))
     
-    if ports:
-        if ports[0] == "small": 
-            ports = psmall
-        elif ports[0] == "large":
-            ports = plarge
 
-    sys.stdout.write(lgreen)
-    print("Domains laoded : ", len(domains), white)
-    print("{:<42}".format('Domain'), " Status  Size       Title / Redirect",end)
     # thread lists
     tlist = []
     for i in range(threadcount):
-        t = threading.Thread(target=work, args=(timeout, ports, is_https, status_codes))
+        t = threading.Thread(target=work, args=(timeout, ports, is_https))
         t.start()
         tlist.append(t)
 
